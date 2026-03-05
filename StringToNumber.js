342e7661281fea79d87018184f1a909b2b9ef052
Piyush Karm <71774702+Piyush-Karn@users.noreply.github.com>
2026-03-05 21:27:23 +0530

    stringtonumber

---
+++ StringToNumber.js
@@ -0,0 +1,4 @@
+let val = prompt("Enter a Number: ");
+let num = Number(val);
+console.log("The input value is: " + val + " and its type is: " + typeof val);
+console.log("The number is: " + num + " and its type is: " + typeof num);
